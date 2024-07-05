d9b75854ee84bf351fda4b850f3eb93923613d8b
simelaneneliswa <emteedethug@gmail.com>
2024-07-05 11:12:35 +0200

    modified config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'students/index'
-  get 'students/show'
-  get 'students/new'
-  get 'students/create'
-  get 'students/edit'
-  get 'students/update'
-  get 'students/destroy'
-  #get 'attendances/index'
-  #get 'attendances/new'
-  #get 'attendances/create'
-  #get 'attendances/edit'
-  #get 'attendances/update'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # Define resources with nested attendances routes
   resources :students do
     resources :attendances, only: [:index, :new, :create, :edit, :update]
   end
 
+  # Define explicit routes for students (optional)
+  # get 'students/index'
+  # get 'students/show'
+  # get 'students/new'
+  # get 'students/create'
+  # get 'students/edit'
+  # get 'students/update'
+  # get 'students/destroy'
+
+  # Define root path
   root 'students#index'
 end
+
